ca86bf884b2cb706670b4262ff39d25c68cc51f4
Khoa Pham <onmyway133@gmail.com>
2017-03-05 21:55:38 +0100

    Add PreferencesViewController

---
+++ GifCapture/Preference/PreferencesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PreferencesViewController.swift
+//  GifCapture
+//
+//  Created by Khoa Pham on 05/03/2017.
+//  Copyright © 2017 Fantageek. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesViewController: NSViewController {
+
+}
