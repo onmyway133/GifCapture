1772376dd78dd1d7d4e47f413f821608e0f11195
Khoa Pham <onmyway133@gmail.com>
2017-06-23 14:50:34 +0200

    Save the frame manually

--- GifCapture/Main/MainWindowController.swift
+++ GifCapture/Main/MainWindowController.swift
@@ -1,11 +1,32 @@
 import AppKit
 
-class MainWindowController: NSWindowController {
+class MainWindowController: NSWindowController, NSWindowDelegate {
+
+  let key = "GifCaptureFrameKey"
 
   override func windowDidLoad() {
     super.windowDidLoad()
 
-    shouldCascadeWindows = false
-    windowFrameAutosaveName = "GifCaptureAutosave"
+    NotificationCenter.default.addObserver(self, selector: #selector(windowWillClose(_:)), name: Notification.Name.NSWindowWillClose, object: nil)
+  }
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+    guard let data = UserDefaults.standard.data(forKey: key),
+      let frame = NSKeyedUnarchiver.unarchiveObject(with: data) as? NSRect else {
+        return
+    }
+
+    window?.setFrame(frame, display: true)
+  }
+
+  func windowWillClose(_ notification: Notification) {
+    guard let frame = window?.frame else {
+      return
+    }
+
+    let data = NSKeyedArchiver.archivedData(withRootObject: frame)
+    UserDefaults.standard.set(data, forKey: key)
   }
 }
